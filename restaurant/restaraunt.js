72c9406c7499ae7db77237cf16d2eb9645821585
Jamie <jxberc@gmail.com>
2014-06-25 14:13:21 -0400

    Add manipulating DOM project

---
+++ restaurant/restaraunt.js
@@ -0,0 +1,47 @@
+
+//generate html content with javascript and jQuery
+
+$("#content").append("<img src='images/logo.png' alt='East Moon Asian Bistro'>")
+$("#content").append("<h1>Welcome to East Moon Asian Bistro!</h1>")
+$("#content").append("<p>Located at 13352 Minnieville Rd., Woodbridge, VA, our restaurant offers a wide array of fine Asian dishes, ranging from traditional dish such as Crispy Duck, Shrimp Tempura, Christmas Roll to new style entrees such as One Night in Bangkok, Veggies Stir-Fry, Sweet Potato Roll.</p>")
+
+var list = '<ul id="tabmenu" >' 
+            + '<li><a class=""'
+            + 'id="tab1">Menu</a></li>' 
+            + '<li><a class=""'
+            + 'id="tab2">Map</a></li>' 
+            + '<li><a class=""'
+            + 'id="tab3">Contact</a></li>' 
+            + '</ul>' 
+            + '<div id="page"></div>'
+$("#content").append(list);
+
+//default tab view
+$("#page").html("<p>Here is our menu: </p> ");
+$("#tab1").addClass("active");
+
+
+//events
+$('#tab1').click(function() {
+  $("#page").empty().html("<p>Here is our menu: </p> ");
+  $("a").removeClass("active");
+  $(this).addClass("active");
+});
+
+$("#tab2").click(function() {
+  $("#page").empty().html("<p>We're located at: </p>");
+  $("a").removeClass("active");
+  $(this).addClass("active");
+});
+
+$("#tab3").click(function() {
+  $("#page").empty().html("<p>We can be reached at: (555)555-5555.</p>");
+  $("a").removeClass("active");
+  $(this).addClass("active");
+});
+
+
+
+
+
+
